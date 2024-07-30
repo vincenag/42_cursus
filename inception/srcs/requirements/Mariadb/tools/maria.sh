1a0d0ec7bcfdf6977a730e949d651c25ffd47915
vincenag <agustin.vincenti92@gmail.com>
2024-07-30 22:54:14 +0200

    Mariadb

---
+++ inception/srcs/requirements/Mariadb/tools/maria.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat << EOF > config.sql
+
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE}:
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+
+EOF
+
+chmod 777 config.sql
+chown -R mysql:root /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+mv config.sql /var/lib/mysql/config.sql
+mariadbd --init-file /var/lib/mysql/config.sql
+
