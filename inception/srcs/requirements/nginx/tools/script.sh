36a983fe0552a11b5202360b3873ce0a2c539d32
Agustin Roberto Vincenti <agvincen@c3r7s1.42malaga.com>
2024-08-16 13:22:38 +0200

    cleaning code

--- inception/srcs/requirements/nginx/tools/script.sh
+++ inception/srcs/requirements/nginx/tools/script.sh
@@ -8,11 +8,5 @@ sed -i "s#__CERTIFICATE__#$CERTIFICATE_#g" /etc/nginx/nginx.conf
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -out $CERTIFICATE_ -keyout $CERTIFICATE_KEY -subj "/C=SP/ST=Andalucia/L=Malaga/O=42/OU=42/CN=$DOMAIN_NAME/UID=$LOGIN_"
 
-# Verificación de archivos generados
-#ls -l $CERTIFICATE_KEY
-#ls -l $CERTIFICATE_
-
-
-# Inicio de nginx
 
 nginx -g 'daemon off;'
--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -1,29 +1,23 @@
 #!/bin/sh
 
-# Descargar y extraer la última versión de WordPress
 wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
 tar -xzf /tmp/wordpress.tar.gz -C /var/www/html
 
-# Crear el directorio para PHP-FPM
 mkdir -p /run/php
 
-# Ajustar permisos para el directorio de WordPress
 chown -R www-data:www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
-# Mover el archivo de configuración predefinido a la ubicación correcta
-#cp ../config/wp-config.php /var/www/html/wordpress/wp-config.php
 
 sed -i "s/__MARIADB_DATABASE__/$MARIADB_DATABASE/" /var/www/html/wordpress/wp-config.php
 sed -i "s/__MARIADB_USER__/$MARIADB_USER/" /var/www/html/wordpress/wp-config.php
 sed -i "s/__MARIADB_PASSWORD__/$MARIADB_PASSWORD/" /var/www/html/wordpress/wp-config.php
 
-# Descargar e instalar WP-CLI
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-# Ejecutar la instalación de WordPress utilizando WP-CLI
 wp core install --allow-root \
   --url=$DOMAIN_NAME \
   --title=Inception \
@@ -32,12 +26,9 @@ wp core install --allow-root \
   --admin_email=$WP_ADMIN_EMAIL \
   --skip-email --path=/var/www/html/wordpress
 
-# Crear un usuario adicional de WordPress
 wp user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD \
   --path=/var/www/html/wordpress --url=$DOMAIN_NAME
 
-# Configurar PHP-FPM para escuchar en el puerto 9000
 sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 9000#g' /etc/php/7.4/fpm/pool.d/www.conf
 
-# Ejecutar PHP-FPM en primer plano
 /usr/sbin/php-fpm7.4 -F
